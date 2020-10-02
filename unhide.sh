fd95c882417cefc560402cacc8308624bb60a353
Ryan Burrow <rsardb11@vt.edu>
2020-10-02 15:11:31 -0400

    Initial commit, these probably have a million bugs

---
+++ unhide.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for entry in "$@"; do
+    src=$(echo $entry | rev | cut -d/ -f1 | rev)
+    if [[ $src == $entry ]]; then
+        path="./"
+    else 
+        path=$(echo $entry | rev | cut -d/ -f2- | rev)
+    fi
+    dst=$(echo $src | cut -d. -f2-)
+    if test -f "$path"/"$dst"; then
+        echo "File already exists, can't hide."
+    else
+        mv "$path"/"$src" "$path"/"$dst"
+    fi
+done
