9615a74959df12c10fb01890906a74cfa6aee03e
Riley Cooper <cooperra@vt.edu>
2020-10-06 19:57:30 -0400

    Added existence check.  Made input cases more clear?

--- unhide.sh
+++ unhide.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 for entry in "$@"; do
-    src=$(echo $entry | rev | cut -d/ -f1 | rev)
-    if [[ $src == $entry ]]; then
-        path="./"
-    else 
-        path=$(echo $entry | rev | cut -d/ -f2- | rev)
-    fi
-    dst=$(echo $src | cut -d. -f2-)
-    if [ "${src:0:1}" != "." ]; then
-        :
-    elif test -f "$path"/"$dst"; then
-        echo "$path/$dst already exists, can't unhide."
+    if test -e $entry; then
+        if [[ $entry == *"/"* ]]; then
+            src=$(echo $entry | rev | cut -d/ -f1 | rev)
+            path=$(echo $entry | rev | cut -d/ -f2- | rev)
+        else
+            src=$entry
+            path="."
+        fi
+        dst=$(echo $src | cut -d. -f2-)
+        if [ "${src:0:1}" != "." ]; then
+            :
+        elif test -f "$path"/"$dst"; then
+            echo "$path/$dst already exists, can't unhide."
+        else
+            mv "$path"/"$src" "$path"/"$dst"
+        fi
     else
-        mv "$path"/"$src" "$path"/"$dst"
+        echo "$entry does not exist"
     fi
 done
